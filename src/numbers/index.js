e930f765332119f761d00bc75112530f526dfa96
sylvia <ojitoscecy.com>
2020-07-10 19:50:49 -0500

    add code

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,28 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+return num % 2 === 0 ? true : false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer);
 }
+
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const arrSum = arr.reduce(reducer);
+  if (arrSum === sum) {
+    return true;
+  } else {
+    return false;
+  }
+} 
 
-}
+
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,17 +1,31 @@
 const split = (str, delim) => {
   // write code for strings.split
+  return str.split(delim);
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let pair = [];
+  for (let i = 0; i < str.length; i+=2){
+    pair.push(str[i] + str[i+1]);
 
+  }
+  return pair;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  let splitWord;
+  let rever;
+  let reversedStr;
 
-}
+  splitWord = str.split('');
+  rever = splitWord.reverse();
+  reversedStr = rever.join('');
+  return reversedStr;
+} 
+
 
 module.exports = {
   split,
